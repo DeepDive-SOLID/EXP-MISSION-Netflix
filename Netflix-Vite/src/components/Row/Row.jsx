08cc862ac8170c9dfb2d7b20e78a947716933996
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-28 16:45:02 +0900

    feat: 무비 열 추가

---
+++ Netflix-Vite/src/components/Row/Row.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import axios from "../../api/axios";
+import styles from "./Row.module.css";
+import MovieModal from "../MovieModal/MovieModal.jsx";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default function Row({ isLargeRow, title, id, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({});
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie);
+  };
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
+  };
+
+  return (
+    <section className={styles.row}>
+      <h2>{title}</h2>
+
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          1378: { slidesPerView: 6, slidesPerGroup: 6 },
+          998: { slidesPerView: 5, slidesPerGroup: 5 },
+          625: { slidesPerView: 4, slidesPerGroup: 4 },
+          0: { slidesPerView: 3, slidesPerGroup: 3 },
+        }}
+      >
+        <div id={id} className={styles.row__posters}>
+          {movies.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <img
+                onClick={() => handleClick(movie)}
+                className={`${styles.row__poster} ${
+                  isLargeRow ? styles.row__posterLarge : ""
+                }`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                loading="lazy"
+                alt={movie.name}
+              />
+            </SwiperSlide>
+          ))}
+        </div>
+      </Swiper>
+
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
+    </section>
+  );
+}
