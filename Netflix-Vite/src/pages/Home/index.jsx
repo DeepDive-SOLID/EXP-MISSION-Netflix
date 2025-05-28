b68ae9d00f10aa11f66759b65782a317b3756713
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-28 16:47:24 +0900

    feat: 검색 자동완성

--- Netflix-Vite/src/pages/Home/index.jsx
+++ Netflix-Vite/src/pages/Home/index.jsx
@@ -1,9 +1,33 @@
+import Banner from "../../components/Banner/Banner.jsx";
 import Nav from "../../components/Nav/Nav.jsx";
+import requests from "../../api/requests.jsx";
+import Row from "../../components/Row/Row.jsx";
 
 export default function Home() {
   return (
     <div>
       <Nav />
+      <Banner />
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 }
