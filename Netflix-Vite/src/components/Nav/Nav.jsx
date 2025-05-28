064224b3ff044a1f2d1ca8468fee38097c74b403
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-28 15:45:25 +0900

    feat : nav바 제작

---
+++ Netflix-Vite/src/components/Nav/Nav.jsx
@@ -0,0 +1,53 @@
+import searchIcon from "../../assets/search.svg";
+import alarmIcon from "../../assets/alarm.svg";
+import userIcon from "../../assets/user.png";
+import styles from "./Nav.module.css";
+import { useState } from "react";
+
+export default function Nav() {
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <div className={styles.nav}>
+      <img
+        alt="Logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        className={styles.nav__logo}
+        onClick={() => window.location.reload()}
+      />
+
+      <ul className={styles.nav__menu}>
+        <li>홈</li>
+        <li>시리즈</li>
+        <li>영화</li>
+        <li>게임</li>
+        <li>New! 요즘 대세 콘텐츠</li>
+        <li>내가 찜한 리스트</li>
+        <li>언어별로 찾아보기</li>
+      </ul>
+
+      <div className={styles.nav__icons}>
+        <img
+          src={searchIcon}
+          alt="Search Icon"
+          className={styles.nav__icon}
+          onClick={() => setShowSearch((prev) => !prev)}
+        />
+
+        {showSearch && (
+          <div className={styles.searchBox}>
+            <img src={searchIcon} alt="검색" className={styles.searchIcon} />
+            <input
+              type="text"
+              className={styles.searchInput}
+              placeholder="제목, 사람, 장르"
+            />
+          </div>
+        )}
+        <span>키즈</span>
+        <img src={alarmIcon} alt="Alarm Icon" className={styles.nav__icon} />
+        <img src={userIcon} alt="User Icon" className={styles.nav__icon} />
+      </div>
+    </div>
+  );
+}
--- Netflix-Vite/src/pages/Home/index.jsx
+++ Netflix-Vite/src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
+import Nav from "../../components/Nav/Nav.jsx";
+
 export default function Home() {
-    return (
-        <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the main page of our application.</p>
-        </div>
-    );
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 }
