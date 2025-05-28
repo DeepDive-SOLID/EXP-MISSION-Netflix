af4f3b14157b27990992ae34b883ec73c34f38cf
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-28 15:04:47 +0900

    initial commit

---
+++ Netflix-Vite/src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
